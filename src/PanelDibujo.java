b3acdaa4b820a08e466ba71efcad7cc1e1bba76e
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-06-20 09:51:52 -0500

    xd

--- src/PanelDibujo.java
+++ src/PanelDibujo.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class PanelDibujo {
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelDibujo extends JPanel {
+    String nom;
+    public void usuario(String nombre){
+        this.nom=nombre;
+    }
+    float peso_us, altura_us, imc_usuario;
+    String usuario=Float.toString(imc_usuario);
+    public void imc(float peso, float altura){
+        this.peso_us=peso;
+        this.altura_us=altura;
+        imc_usuario=peso_us/(altura_us*altura_us);
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int anchura=getWidth();
+        int altura=getHeight();
+        /*g.drawLine(0,0,anchura,altura);
+        g.drawLine(0,altura,anchura,0);*/
+        /*g.drawLine(10,0,10,altura);
+        g.drawLine(10,0,250,250);
+        g.drawLine(440,0,250,250);
+        g.drawLine(440,0,440,altura);*/
+        //g.drawRoundRect(0,0,50,50,30,30);
+        //g.drawString("jp",20,20);
+        //g.drawString(nom,400,400);
+        g.drawString(usuario,400,400);
+
+    }
 }
--- src/PruebaPanelDibujo.java
+++ src/PruebaPanelDibujo.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class PruebaPanelDibujo {
+import javax.swing.*;
+import java.util.Scanner;
+
+public class PruebaPanelDibujo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //creo una instancia de la clase PanelDibujo
+        PanelDibujo panel = new PanelDibujo();
+        //creo un marco en la ventana
+        JFrame aplicacion = new JFrame();
+        //pongo un boton para cerrar la ventana
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*String nombre;
+        System.out.printf("Ingrese su nombre: ");
+        nombre=entrada.nextLine();
+        panel.usuario(nombre);*/
+
+        float peso, altura;
+        System.out.printf("Ingrese su peso en kg: ");
+        peso= entrada.nextFloat();
+        System.out.printf("Ingrese su altura en m: ");
+        altura= entrada.nextFloat();
+        panel.imc(peso,altura);
+
+        aplicacion.add(panel);//agrego un "panel" a la ventana
+        aplicacion.setSize(800,800);//seteo el tamaño de la ventana
+        aplicacion.setVisible(true);//permito que se vea la ventana
+
+    }
 }
